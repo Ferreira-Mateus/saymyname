12d794ad5d1429fd2930232ad5253973af618354
matheusarmando199 <matheus.narutofam@gmail.com>
2022-05-29 15:27:17 -0300

    configuração inicial do projeto

---
+++ sayMyName/sayMyName/sayMyNameMM.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  sayMyName
+//
+//  Created by Matheus Armando on 29/05/22.
+//
+
+import UIKit
+
+class sayMyNameMM: UIViewController {
+    
+    
+    // Elementos
+    
+    // Funcionalidades
+    
+    // Constraints
+    
+    // Metodos inicializadores
+    
+    // Ciclos de vida
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPurple
+    }
+
+
+}
+
